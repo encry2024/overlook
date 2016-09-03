27789064a64ace15df11df686d6e183bc5ff5a35
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-03 08:31:56 +0800

    Completed overlook's customer page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,58 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="col-lg-12">
+        <div class="row">
+            @include('layouts.sidebar')
+            <div class="col-lg-9 col-md-8">
+                <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" style="font-size: 22px;">DASHBOARD</div>
+                    </div>
+                </div>
 
-                <div class="panel-body">
-                    You are logged in!
+                <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-body">
+                            <div id="calendar"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+
+        $('#calendar').fullCalendar({
+            selectable: false,
+            height: 500,
+            header: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'month'
+            },
+
+/*            select: function( start, end) {
+                var check = start.format();
+                var today = moment().format();
+
+                if(check < today) {
+                } else {
+
+                }
+            },
+
+            eventClick: function(event) {
+                if (event.url) {
+                    window.open(event.url);
+                    return false;
+                }
+            },*/
+            editable: true
+        });
+    });
+</script>
 @endsection
