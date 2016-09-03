7f80977f21fc8c551a5d7a3dbe60620e5b0bec0d
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-03 08:45:39 +0800

    Added auth group

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/homepage', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/dashboard', 'HomeController@index');
+});
