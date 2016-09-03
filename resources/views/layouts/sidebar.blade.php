4b4deacbdf6e9c6073f0d1ec754fd6fb9014c5f0
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2016-09-03 08:43:10 +0800

    Added sidebar file for flexibility

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="col-lg-3">
+    <div class="list-group">
+        <label href="#" class="list-group-item active">
+            <i class="fa fa-tachometer"></i> Dashboard
+        </label>
+        <a href="#" class="list-group-item"><i class="fa fa-book"></i>&nbsp;&nbsp;Reservations</a>
+        <a href="#" class="list-group-item"><i class="fa fa-list"></i>&nbsp;&nbsp;Room Categories</a>
+        <a href="#" class="list-group-item"><i class="fa fa-flag"></i>&nbsp;&nbsp;Pools</a>
+        <a href="#" class="list-group-item"><i class="fa fa-diamond"></i>&nbsp;&nbsp;Amenities</a>
+        <a href="#" class="list-group-item"><i class="fa fa-money"></i>&nbsp;&nbsp;Entrance Fees</a>
+        <a href="#" class="list-group-item"><i class="fa fa-users"></i>&nbsp;&nbsp;Users</a>
+        <a href="#" class="list-group-item"><i class="fa fa-briefcase"></i>&nbsp;&nbsp;Reports</a>
+    </div>
+</div>
